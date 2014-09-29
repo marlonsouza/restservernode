d443f4d9c8f84d752b426b9991d0bf2a08a206b0
Márlon <marlon.souuza@gmail.com>
2014-09-28 23:36:50 -0300

    Retirando metodo de cabeçalho do httputil.

--- common/httputil.js
+++ common/httputil.js
@@ -6,14 +6,5 @@ module.exports =  {
             err: objError.code,
             err_type: strType
        });
-    },
-
-    defineHeaderResponse: function(req, res){
-
-          res.header('Access-Control-Allow-Origin', '*');
-          res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type');
-          res.header('AAccess-Control-Expose-Headers', 'Location, Content-Disposition');
-          res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE, HEAD, OPTIONS');
-
     }
 }
